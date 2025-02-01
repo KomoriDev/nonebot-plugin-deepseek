ce2656bcfdc12815c55c1017f3ed67def0b3bbc2
LonelySail <xiaocaicai_email@sina.com>
2025-02-01 21:57:38 +0800

    优化代码。

--- nonebot_plugin_deepseek/apis/request.py
+++ nonebot_plugin_deepseek/apis/request.py
@@ -1,6 +1,5 @@
-import json
 import asyncio
-from asyncio.queues import Queue
+from json import loads
 
 from httpx import AsyncClient
 from nonebot.log import logger
@@ -22,51 +21,39 @@ class API:
     @classmethod
     async def chat(cls, message: list[dict[str, str]], model: str = "deepseek-chat") -> ChatChunkedCompletions:
         """普通对话"""
-        chunk_queue = Queue()
-
-        async def send_request():
-            model_config = config.get_model_config(model)
-            json = {
-                "stream": True,
-                "model": model,
-                "messages": [{"content": config.prompt, "role": "user"}] + message if config.prompt else message,
-                **model_config.to_dict(),
-            }
-            logger.debug(f"使用模型 {model}，配置：{json}")
-            async with cls._client.stream(
-                "POST", f"{model_config.base_url}/chat/completions", json=json, headers=cls._headers + {"Content-Type": "application/json"}
-            ) as response:
-                async for chunk in response.aiter_lines():
-                    await chunk_queue.put(chunk)
-
+        model_config = config.get_model_config(model)
         # if model == "deepseek-chat":
         #     json.update({"tools": registry.to_json()})
-        
-        result_model = None
-        result_message = ""
-
-        request_task = asyncio.create_task(send_request())
-
-        while True:
-            chunk = await asyncio.wait_for(chunk_queue.get(), timeout=10)
-            if not chunk:
-                continue
-            chunk = chunk.lstrip('data:').strip()
-            if chunk == "[DONE]":
-                break
-            data = json.loads(chunk)
-            if error := data.get("error"):
-                request_task.cancel()
-                raise RequestException(error["message"])
-            if data.get("usage"):
-                data["choices"][0].pop("delta")
-                data["choices"][0]["message"] = result_message
-                result_model = ChatChunkedCompletions(**data)
-                continue
-            result_message += data["choices"][0]["delta"]["content"]
-        if not request_task.done():
-            request_task.cancel()
-        return result_model
+        json = {
+            "stream": True,
+            "model": model,
+            "messages": [{"content": config.prompt, "role": "user"}] + message if config.prompt else message,
+            **model_config.to_dict(),
+        }
+        logger.debug(f"使用模型 {model}，配置：{json}")
+        async with cls._client.stream(
+            "POST", f"{model_config.base_url}/chat/completions", json=json, headers=cls._headers + {"Content-Type": "application/json"}
+        ) as response:
+            result_model = None
+            result_message = ""
+            stream_iterator = response.aiter_lines()
+            while True:
+                chunk = await asyncio.wait_for(anext(stream_iterator), timeout=10)
+                if not chunk:
+                    continue
+                chunk = chunk.lstrip('data:').strip()
+                if chunk == "[DONE]":
+                    break
+                data = loads(chunk)
+                if error := data.get("error"):
+                    raise RequestException(error["message"])
+                if data.get("usage"):
+                    data["choices"][0].pop("delta")
+                    data["choices"][0]["message"] = result_message
+                    result_model = ChatChunkedCompletions(**data)
+                    continue
+                result_message += data["choices"][0]["delta"]["content"]
+            return result_model
 
     @classmethod
     async def query_balance(cls) -> Balance:
